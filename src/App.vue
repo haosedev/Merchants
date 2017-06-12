16b9bf4541352e9cdd3a3e97e7a3922ea3cb559d
Haosedev <530178@qq.com>
2017-06-12 22:47:40 +0800

    home commit

--- src/App.vue
+++ src/App.vue
@@ -37,28 +37,9 @@
 </template>
 
 <script>
-import store from '@/vuex/store'
-import {
-    Actionsheet,
-    ButtonTab,
-    ButtonTabItem,
-    ViewBox,
-    XHeader,
-    Tabbar,
-    TabbarItem,
-    Loading,
-    TransferDom
-} from 'vux'
-import {
-    mapState,
-    mapActions
-} from 'vuex'
-
-export default {
-    directives: {
-        TransferDom
-    },
-    components: {
+    import store from '@/vuex/store'
+    import {
+        Actionsheet,
         ButtonTab,
         ButtonTabItem,
         ViewBox,
@@ -66,248 +47,270 @@ export default {
         Tabbar,
         TabbarItem,
         Loading,
-        Actionsheet
-    },
-    methods: {
-        onClickMore() {
-            this.showMenu = true
+        TransferDom
+    } from 'vux'
+    import {
+        mapState,
+        mapActions
+    } from 'vuex'
+
+    export default {
+        directives: {
+            TransferDom
         },
-        menuclick(key) {
-            switch (key) {
-                case 'menu1':
-                    this.$router.push('/');
-                    break;
-                case 'menu2':
-                    this.$router.push('/h2');
-                    break;
-                case 'menu3':
-                    console.log('3 unset');
-                    break;
-                case 'menu4':
-                    console.log('4 unset');
-                    break;
-                case 'menu5':
-                    console.log('5 unset');
-                    break;
-                default:
-                    console.log('default menu');
-            }
+        components: {
+            ButtonTab,
+            ButtonTabItem,
+            ViewBox,
+            XHeader,
+            Tabbar,
+            TabbarItem,
+            Loading,
+            Actionsheet
         },
-        ...mapActions([
-            'updateDemoPosition'
-        ])
-    },
-    mounted() {
-        this.handler = () => {
-            if (this.path === '/demo') {
-                this.box = document.querySelector('#demo_list_box')
-                this.updateDemoPosition(this.box.scrollTop)
-            }
-        }
-    },
-    beforeDestroy() {
-        this.box.removeEventListener('scroll', this.handler, false)
-    },
-    watch: {
-        // path(path) {
-        //     if (path === '/') {
-        //         this.$router.replace('/')
-        //         return
-        //     }
-        //     if (path === '/') {
-        //         setTimeout(() => {
-        //             this.box = document.querySelector('#demo_list_box')
-        //             if (this.box) {
-        //                 this.box.removeEventListener('scroll', this.handler, false)
-        //                 this.box.addEventListener('scroll', this.handler, false)
-        //             }
-        //         }, 1000)
-        //     } else {
-        //         this.box && this.box.removeEventListener('scroll', this.handler, false)
-        //     }
-        // }
-    },
-    computed: {
-        ...mapState({
-            route: state => state.route,
-            path: state => state.route.path,
-            deviceready: state => state.app.deviceready,
-            demoTop: state => state.demoScrollTop,
-            isLoading: state => state.isLoading,
-            direction: state => state.direction
-        }),
-        isShowBar() {
-            if (/component/.test(this.path)) {
-                return true
-            }
-            return false
-        },
-        leftOptions() {
-            return {
-                showBack: this.route.path !== '/'
-            }
-        },
-        rightOptions() {
-            return {
-                showMore: true
-            }
-        },
-        headerTransition() {
-            return this.direction === 'forward' ? 'vux-header-fade-in-right' : 'vux-header-fade-in-left'
-        },
-        componentName() {
-            if (this.route.path) {
-                const parts = this.route.path.split('/')
-                if (/component/.test(this.route.path) && parts[2]) return parts[2]
-            }
-        },
-        isDemo() {
-            return /h2/.test(this.route.path)
-        },
-        isTabbarDemo() {
-            return /tabbar/.test(this.route.path)
-        },
-        title() {
-            if (this.route.path === '/') return '首页'
-            return this.componentName ? `${this.componentName}` : 'Merchant'
-        }
-    },
-    data() {
-        return {
-            showMenu: false,
-            menus: {
-                menu1: '首页',
-                menu2: '购物车',
-                menu3: '列表',
-                menu4: '仓储',
-                menu5: '系统设置',
+        methods: {
+            onClickMore() {
+                this.showMenu = true
             },
+            menuclick(key) {
+                switch (key) {
+                    case 'menu1':
+                        this.$router.push('/');
+                        break;
+                    case 'menu2':
+                        this.$router.push('/h2');
+                        break;
+                    case 'menu3':
+                        console.log('3 unset');
+                        break;
+                    case 'menu4':
+                        console.log('4 unset');
+                        break;
+                    case 'menu5':
+                        console.log('5 unset');
+                        break;
+                    default:
+                        console.log('default menu');
+                }
+            },
+            ...mapActions([
+                'updateDemoPosition'
+            ])
+        },
+        mounted() {
+            this.handler = () => {
+                if (this.path === '/demo') {
+                    this.box = document.querySelector('#demo_list_box')
+                    this.updateDemoPosition(this.box.scrollTop)
+                }
+            }
+        },
+        beforeDestroy() {
+            this.box.removeEventListener('scroll', this.handler, false)
+        },
+        watch: {
+            // path(path) {
+            //     if (path === '/') {
+            //         this.$router.replace('/')
+            //         return
+            //     }
+            //     if (path === '/') {
+            //         setTimeout(() => {
+            //             this.box = document.querySelector('#demo_list_box')
+            //             if (this.box) {
+            //                 this.box.removeEventListener('scroll', this.handler, false)
+            //                 this.box.addEventListener('scroll', this.handler, false)
+            //             }
+            //         }, 1000)
+            //     } else {
+            //         this.box && this.box.removeEventListener('scroll', this.handler, false)
+            //     }
+            // }
+        },
+        computed: {
+            ...mapState({
+                route: state => state.route,
+                path: state => state.route.path,
+                deviceready: state => state.app.deviceready,
+                demoTop: state => state.demoScrollTop,
+                isLoading: state => state.isLoading,
+                direction: state => state.direction
+            }),
+            isShowBar() {
+                if (/component/.test(this.path)) {
+                    return true
+                }
+                return false
+            },
+            leftOptions() {
+                return {
+                    showBack: this.route.path !== '/'
+                }
+            },
+            rightOptions() {
+                return {
+                    showMore: true
+                }
+            },
+            headerTransition() {
+                return this.direction === 'forward' ? 'vux-header-fade-in-right' : 'vux-header-fade-in-left'
+            },
+            componentName() {
+                if (this.route.path) {
+                    const parts = this.route.path.split('/')
+                    if (/component/.test(this.route.path) && parts[2]) return parts[2]
+                }
+            },
+            isDemo() {
+                return /h2/.test(this.route.path)
+            },
+            isTabbarDemo() {
+                return /tabbar/.test(this.route.path)
+            },
+            title() {
+                if (this.route.path === '/') return '首页'
+                return this.componentName ? `${this.componentName}` : 'Merchant'
+            }
+        },
+        data() {
+            return {
+                showMenu: false,
+                menus: {
+                    menu1: '首页',
+                    menu2: '购物车',
+                    menu3: '列表',
+                    menu4: '仓储',
+                    menu5: '系统设置',
+                },
+                version: '0.1.5',
+            }
         }
     }
-}
 </script>
 
 <style lang="less">
-@import '~vux/src/styles/reset.less';
-@import '~vux/src/styles/1px.less';
-@import '~vux/src/styles/tap.less';
-body {
-    background-color: #fbf9fe;
-}
-
-html,
-body {
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-}
-
-.icon {
-    font-family: "iconfont" !important;
-    font-size: 30px;
-    color: #888;
-}
-.black{
-    color:#000;
-}
-.weui-tabbar.vux-demo-tabbar {
-    /** backdrop-filter: blur(10px);
+    @import '~vux/src/styles/reset.less';
+    @import '~vux/src/styles/1px.less';
+    @import '~vux/src/styles/tap.less';
+    body {
+        background-color: #fbf9fe;
+    }
+    
+    html,
+    body {
+        height: 100%;
+        width: 100%;
+        overflow-x: hidden;
+    }
+    
+    .icon {
+        font-family: "iconfont" !important;
+        font-size: 30px;
+        color: #888;
+    }
+    
+    .black {
+        color: #000;
+    }
+    
+    .weui-tabbar.vux-demo-tabbar {
+        /** backdrop-filter: blur(10px);
   background-color: none;
   background: rgba(247, 247, 250, 0.5);**/
-}
-
-.vux-demo-tabbar .weui-bar__item_on .icon {
-    color: #0275d8;
-}
-
-.vux-demo-tabbar .weui-tabbar_item.weui-bar__item_on .vux-demo-tabbar-icon-home {
-    color: rgb(53, 73, 94);
-}
-
-.icon:before {
-    content: attr(icon);
-}
-
-.vux-demo-tabbar-component {
-    background-color: #F70968;
-    color: #fff;
-    border-radius: 7px;
-    padding: 0 4px;
-    line-height: 14px;
-}
-
-.weui-tabbar__icon+.weui-tabbar__label {
-    margin-top: 0!important;
-}
-
-.vux-demo-header-box {
-    z-index: 100;
-    position: absolute;
-    width: 100%;
-    left: 0;
-    top: 0;
-}
-
-@font-face {
-    font-family: 'vux-demo';
-    /* project id 70323 */
-    src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot');
-    src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot?#iefix') format('embedded-opentype'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.woff') format('woff'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.ttf') format('truetype'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.svg#iconfont') format('svg');
-}
-
-.demo-icon {
-    font-family: 'vux-demo';
-    font-size: 20px;
-    color: #04BE02;
-}
-
-.demo-icon-big {
-    font-size: 28px;
-}
-
-.demo-icon:before {
-    content: attr(icon);
-}
-
-.router-view {
-    width: 100%;
-    top: 46px;
-}
-
-.vux-pop-out-enter-active,
-.vux-pop-out-leave-active,
-.vux-pop-in-enter-active,
-.vux-pop-in-leave-active {
-    will-change: transform;
-    transition: all 500ms;
-    height: 100%;
-    top: 46px;
-    position: absolute;
-    backface-visibility: hidden;
-    perspective: 1000;
-}
-
-.vux-pop-out-enter {
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-}
-
-.vux-pop-out-leave-active {
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-}
-
-.vux-pop-in-enter {
-    opacity: 0;
-    transform: translate3d(100%, 0, 0);
-}
-
-.vux-pop-in-leave-active {
-    opacity: 0;
-    transform: translate3d(-100%, 0, 0);
-}
-
-.menu-title {
-    color: #888;
-}
+    }
+    
+    .vux-demo-tabbar .weui-bar__item_on .icon {
+        color: #0275d8;
+    }
+    
+    .vux-demo-tabbar .weui-tabbar_item.weui-bar__item_on .vux-demo-tabbar-icon-home {
+        color: rgb(53, 73, 94);
+    }
+    
+    .icon:before {
+        content: attr(icon);
+    }
+    
+    .vux-demo-tabbar-component {
+        background-color: #F70968;
+        color: #fff;
+        border-radius: 7px;
+        padding: 0 4px;
+        line-height: 14px;
+    }
+    
+    .weui-tabbar__icon+.weui-tabbar__label {
+        margin-top: 0!important;
+    }
+    
+    .vux-demo-header-box {
+        z-index: 100;
+        position: absolute;
+        width: 100%;
+        left: 0;
+        top: 0;
+    }
+    
+    @font-face {
+        font-family: 'vux-demo';
+        /* project id 70323 */
+        src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot');
+        src: url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.eot?#iefix') format('embedded-opentype'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.woff') format('woff'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.ttf') format('truetype'), url('https://at.alicdn.com/t/font_h1fz4ogaj5cm1jor.svg#iconfont') format('svg');
+    }
+    
+    .demo-icon {
+        font-family: 'vux-demo';
+        font-size: 20px;
+        color: #04BE02;
+    }
+    
+    .demo-icon-big {
+        font-size: 28px;
+    }
+    
+    .demo-icon:before {
+        content: attr(icon);
+    }
+    
+    .router-view {
+        width: 100%;
+        top: 46px;
+    }
+    
+    .vux-pop-out-enter-active,
+    .vux-pop-out-leave-active,
+    .vux-pop-in-enter-active,
+    .vux-pop-in-leave-active {
+        will-change: transform;
+        transition: all 500ms;
+        height: 100%;
+        top: 46px;
+        position: absolute;
+        backface-visibility: hidden;
+        perspective: 1000;
+    }
+    
+    .vux-pop-out-enter {
+        opacity: 0;
+        transform: translate3d(-100%, 0, 0);
+    }
+    
+    .vux-pop-out-leave-active {
+        opacity: 0;
+        transform: translate3d(100%, 0, 0);
+    }
+    
+    .vux-pop-in-enter {
+        opacity: 0;
+        transform: translate3d(100%, 0, 0);
+    }
+    
+    .vux-pop-in-leave-active {
+        opacity: 0;
+        transform: translate3d(-100%, 0, 0);
+    }
+    
+    .menu-title {
+        color: #888;
+    }
 </style>
